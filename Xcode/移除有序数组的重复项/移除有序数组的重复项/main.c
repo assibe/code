b6a76d72f8965e9416b3182275f0c0540f4d2ea4
assibe <2775213538@qq.com>
2022-03-13 16:04:41 +0800

    提交代码

---
+++ Xcode/移除有序数组的重复项/移除有序数组的重复项/main.c
@@ -0,0 +1,46 @@
+//
+//  main.c
+//  删除有序数组中的重复项
+//
+//  Created by zhourenxi on 2022/3/12.
+//
+
+#include <stdio.h>
+#include <assert.h>
+
+int removeDuplicates(int* nums, int numsSize){
+    assert(nums);
+    int left = 0;
+    int dest = 0;
+    int right = 1;
+    
+    for (right = 1; right < numsSize; right++) {
+         
+        while (nums[left] == nums[right]) {
+            right++;
+        }
+        nums[dest++] = nums [left];
+        left = right;
+    }
+    
+    nums[dest++] = nums[left];
+    
+    return dest ;
+};
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    int nums[] = {1,1,2}; // 输入数组
+    int numSize = sizeof(nums) / sizeof(int);
+    printf("%d",numSize);
+    
+    int expectedNums = 2; // 长度正确的期望答案
+    
+    if (expectedNums == removeDuplicates(nums,numSize)) {
+        printf("成功");
+    }
+    return 0;
+}
+
