5a4fb55fa383d7cfe3e65b8e5bca5e740cd74846
assibe <2775213538@qq.com>
2022-03-18 00:06:43 +0800

    提交代码

--- Xcode/移除链表元素/移除链表元素/main.c
+++ Xcode/移除链表元素/移除链表元素/main.c
@@ -6,6 +6,8 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -13,7 +15,56 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
+struct ListNode {
+    int val;
+    struct ListNode *next;
+    
+};
 
 struct ListNode* removeElements(struct ListNode* head, int val){
+   
+    assert(head);
+    
+    struct ListNode *cur = head;
+    
+    while (head -> val == val) {//排除头指针
+        
+        head = head -> next;
+        free(cur);
+        cur = head;
+        
+        if (cur -> next == NULL) {//
+            return head;
+        }
+    }
+    
+    struct ListNode *prev = head;
+    cur = cur ->next;
+    
+    while (cur) {
+        
+        if (cur->val == val) {
+          
+            prev ->next = cur->next;
+            free(cur);
+            cur = NULL;
+            
+            if (prev -> next == NULL) {
+                return head;
+            }
+            
+            prev = prev -> next;
+            cur = cur->next;
+            
+        }
+        else{
+            
+            prev = prev ->next;
+            cur = cur -> next;
+            
+        }
+            
+    }
+    return head;
     
 };
