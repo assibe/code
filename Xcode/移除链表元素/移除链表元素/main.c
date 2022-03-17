7f9e8e6a714b4fd5412c0949a9ecbcb160bce08d
assibe <2775213538@qq.com>
2022-03-17 17:57:33 +0800

    提交代码

---
+++ Xcode/移除链表元素/移除链表元素/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  移除链表元素
+//
+//  Created by zhourenxi on 2022/3/17.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
+
+struct ListNode* removeElements(struct ListNode* head, int val){
+    
+};
