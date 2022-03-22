fe57983d2f59b36ce005dc48ab5b94e104d4f9fe
assibe <2775213538@qq.com>
2022-03-22 08:17:09 +0800

    提交代码

--- Xcode/合并两个有序链表/合并两个有序链表/main.c
+++ Xcode/合并两个有序链表/合并两个有序链表/main.c
@@ -6,9 +6,32 @@
 //
 
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
     return 0;
+}
+
+
+struct ListNode {
+    
+    int val;
+    struct ListNode *next;
+    
+};
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
+    
+    if (list1 == NULL) {
+        
+        return list1;
+    }
+    else{
+        if (list2) {
+            return list2;
+        }
+    }
+    
+    struct ListNode *head = NULL,*tail = NULL;
 }
--- Xcode/链表中倒数第k个结点/链表中倒数第k个结点/main.c
+++ Xcode/链表中倒数第k个结点/链表中倒数第k个结点/main.c
@@ -19,7 +19,31 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
+//题思路：
+//快慢指针法 fast, slow, 首先让fast先走k步，然后fast,slow同时走，fast走到末尾时，slow走到倒数第k个节点。
+//*/
+
 struct ListNode* FindKthToTail(struct ListNode* pListHead, int k ) {
     // write code here
+    
+    struct ListNode *fast = pListHead;
+    struct ListNode *low = pListHead;
+    
+    while (k--) {
+        
+        if (fast) {
+            fast =fast -> next;
+        }
+        else{
+            return NULL;
+        }
+            
+    }
+    
+    while (fast) {
+        fast = fast->next;
+        low = low -> next;
+    }
+    return low;
     
 }
