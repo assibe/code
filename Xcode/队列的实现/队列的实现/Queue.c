78a5a5d2c2d965725c119a1c0ebbc33bc5bc2400
assibe <2775213538@qq.com>
2022-03-30 00:02:17 +0800

    代码提交

--- Xcode/队列的实现/队列的实现/Queue.c
+++ Xcode/队列的实现/队列的实现/Queue.c
@@ -6,3 +6,106 @@
 //
 
 #include "Queue.h"
+
+void QueueInit(Queue* pq){//初始化
+    
+    assert(pq);
+    pq ->tail = pq -> tail = NULL;
+}
+
+void QueuePop(Queue* pq){
+    
+    assert(pq);
+    assert(pq ->head &&pq ->tail);
+    QNode *next = pq -> head -> next;
+    free(pq ->head);
+    pq -> head = next;
+    
+}
+
+void QueueDestory(Queue* pq){
+    
+    assert(pq);
+    QNode *cur = pq ->head;
+    while (cur) {
+        
+        cur = cur->next;
+        free(pq ->head);
+        pq -> head = cur;
+    }
+    
+    free(pq);
+    pq = NULL;
+    pq ->head = pq -> tail = NULL;
+}
+
+QNode *MakeNode(QDataType x){
+    
+    QNode * cur = malloc(sizeof(QNode));
+    assert(cur);
+    cur ->data = x;
+    cur ->next = NULL;
+    return cur;
+}
+
+void QueuePush(Queue* pq, QDataType x){
+    
+    if (pq ->head == NULL ) {
+        pq -> head = pq -> tail = MakeNode(x);
+    }
+    else{
+        pq -> tail -> next = MakeNode(x);
+        pq -> tail = pq ->tail ->next;
+    }
+}
+
+void QueuePoP(Queue* pq){
+    
+    assert(pq);
+    assert(pq ->head&&pq -> tail);
+    if (pq -> head ->next == NULL) {
+        free(pq ->head);
+        pq ->head = pq ->tail = NULL;
+    }
+    else{
+        QNode *next = pq -> head -> next;
+        free(pq ->head);
+        pq -> head = next;
+    }
+    
+}
+
+bool QueueEmpty(Queue* pq){
+    assert(pq);
+    return pq -> head ==NULL;
+}
+
+size_t QueueSize(Queue* pq){
+    
+    assert(pq);
+    QNode *cur = pq -> head;
+    size_t Size = 0;
+    while (cur) {
+        Size++;
+        cur = cur ->next;
+    }
+    
+    return Size;
+}
+
+QDataType QueueFront(Queue* pq){
+    
+    assert(pq);
+    assert(pq -> head);
+    return pq ->head ->data;
+    
+}
+
+QDataType QueueBack(Queue* pq){
+    
+    assert(pq);
+    assert(pq ->head);
+    
+    return pq -> tail ->data;
+}
+
--- Xcode/队列的实现/队列的实现/Queue.h
+++ Xcode/队列的实现/队列的实现/Queue.h
@@ -11,36 +11,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <assert.h>
 
 #endif /* Queue_h */
 
-// 链式结构：表示队列
-typedef struct QListNode
+typedef int QDataType;
+
+typedef struct QueueNode
 {
-    struct QListNode* _next;
-    QDataType _data;
+    QDataType data;
+    struct QueueNode* next;
 }QNode;
- 
-// 队列的结构
+
 typedef struct Queue
 {
-    QNode* _front;
-    QNode* _rear;
+    QNode* head;
+    QNode* tail;
+
+    //size_t size;
 }Queue;
- 
-// 初始化队列
-void QueueInit(Queue* q);
-// 队尾入队列
-void QueuePush(Queue* q, QDataType data);
-// 队头出队列
-void QueuePop(Queue* q);
-// 获取队列头部元素
-QDataType QueueFront(Queue* q);
-// 获取队列队尾元素
-QDataType QueueBack(Queue* q);
-// 获取队列中有效元素个数
-int QueueSize(Queue* q);
-// 检测队列是否为空，如果为空返回非零结果，如果非空返回0
-int QueueEmpty(Queue* q);
-// 销毁队列
-void QueueDestroy(Queue* q);
+
+void QueueInit(Queue* pq);
+void QueueDestory(Queue* pq);
+void QueuePush(Queue* pq, QDataType x);
+void QueuePop(Queue* pq);
+bool QueueEmpty(Queue* pq);
+size_t QueueSize(Queue* pq);
+ QDataType QueueFront(Queue* pq);
+QDataType QueueBack(Queue* pq);
