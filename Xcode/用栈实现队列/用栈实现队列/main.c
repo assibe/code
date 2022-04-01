3d10802fcd3d926d8693763a799e7ed5da12a5b7
assibe <2775213538@qq.com>
2022-04-01 13:02:32 +0800

    提交代码

---
+++ Xcode/用栈实现队列/用栈实现队列/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  用栈实现队列
+//
+//  Created by zhourenxi on 2022/4/1.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ Xcode/用队列实现栈/用队列实现栈/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  用队列实现栈
+//
+//  Created by zhourenxi on 2022/3/30.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
