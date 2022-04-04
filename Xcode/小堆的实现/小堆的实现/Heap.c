663a0c56f3741b8522707cd928dc45b60766db5b
assibe <2775213538@qq.com>
2022-04-05 01:15:38 +0800

    提交代码

--- Xcode/小堆的实现/小堆的实现/Heap.c
+++ Xcode/小堆的实现/小堆的实现/Heap.c
@@ -40,5 +40,106 @@ void HeapDestroy(struct Heap *php){
 
 
 void HeapPrint(struct Heap *php){
+    assert(php);
     
+    int n = 0;
+    while (n<php->size) {
+        
+        printf("%d",php->a[n]);
+        n++;
+    }
+}
+
+
+void AdjustUp(HeapData *pa,size_t child){
+    
+    size_t parent = (child -1)/2;
+    
+    while (parent > 0) {
+        
+        if (pa[parent]<pa[child]) {
+            
+            int tmp = pa[child];
+            pa[child] = pa[parent];
+            pa[parent] = tmp;
+            
+            child = parent;
+            parent = (child - 1)/2;
+        }
+        else
+            break;
+    }
+}
+
+
+
+void HeapPush(struct Heap *php,HeapData x){
+    assert(php);
+    
+    if (php->size == php->capacity) {//判断是否扩容
+        
+        size_t newcapacity = php->capacity == 0?4:php->capacity*2;
+        HeapData *tmp = realloc(php->a, sizeof(HeapData));
+        
+        if (tmp == NULL) {
+            printf("reamlloc fail");
+            exit(-1);
+        }
+        
+        php->a = tmp;
+        php->capacity = newcapacity;
+    }
+    
+    php->a[php->size++] = x;
+    
+    AdjustUp(php->a, php->size -1);
+}
+
+void AdjustDown(HeapData*a,size_t size,size_t root){//root为调制开始的位置
+    
+    size_t parent = root;
+    size_t child = parent *2 + 1;
+    
+    while (child < size) {
+        
+        if (child + 1 < size && a[child + 1] < a[child]) {
+            HeapData tmp = a[child];
+            a[child] = a[parent];
+            a[parent] = tmp;
+            
+            parent = child;
+            child = parent*2 - 1;
+            
+        }
+        else
+            break;
+    }
+}
+
+
+void HeapPop(struct Heap*php){
+    assert(php);
+    
+    HeapData tmp = php->a[0];
+    php->a[0]  = php->a[php->size-1];
+    php->a[(php->size--)-1] = tmp;
+    
+    AdjustDown(php->a,php->size,0);
+    
+}
+
+void HeapSort(HeapData *a,size_t size){//堆排序
+    struct Heap hp ;
+    HeapInit(&hp);
+    for (int  i = 0; i < size; i++) {
+        HeapPush(&hp, a[i]);
+    }
+    
+    size_t j = 0;
+    while (!HeapEmpty(&hp)) {
+        
+        a[j++] = HeapTop(&hp);
+        HeapPop(&hp);
+    }
+    HeapDestroy(&hp);
 }
--- Xcode/小堆的实现/小堆的实现/Heap.h
+++ Xcode/小堆的实现/小堆的实现/Heap.h
@@ -19,8 +19,8 @@ typedef int HeapData;
 
 struct Heap{
     HeapData *a;
-    int size;
-    int capacity;
+    size_t size;
+    size_t capacity;
 };
 
 void HeapInit(struct Heap*php);
@@ -34,3 +34,11 @@ HeapData HeapTop(struct Heap *php);
 void HeapDestroy(struct Heap *php);
 
 void HeapPrint(struct Heap *php);
+
+void HeapPush(struct Heap *php,HeapData x);
+
+void AdjustUp(HeapData *pa,size_t child);
+
+void HeapPop(struct Heap*php);
+
+void HeapSort(HeapData *a,size_t size);
