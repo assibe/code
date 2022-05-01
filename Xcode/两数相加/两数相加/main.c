abba0f8655507a03402ee382880360ac6accb0e0
assibe <2775213538@qq.com>
2022-05-01 22:54:42 +0800

    提交代码

---
+++ Xcode/两数相加/两数相加/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  两数相加
+//
+//  Created by zhourenxi on 2022/5/1.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
