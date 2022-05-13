4242f0cb033e36848c27e48cef6641d68f287cd2
assibe <2775213538@qq.com>
2022-05-13 23:17:47 +0800

    提交代码

---
+++ Xcode/c++的学习/DerivedData/c++的学习/Build/Intermediates.noindex/c++的学习.build/Debug/c++的学习.build/Objects-normal/arm64/main.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/zhourenxi/Desktop/code/Xcode/c++的学习/c++的学习/main.cpp
---
+++ Xcode/c++的学习/c++的学习/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c++的学习
+//
+//  Created by zhourenxi on 2022/5/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    int *n = nullptr;
+    return 0;
+}
