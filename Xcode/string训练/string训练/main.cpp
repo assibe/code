c3e125f13971f6bf9232ba899cfaaaf661005187
assibe <2775213538@qq.com>
2022-07-07 23:11:51 +0800

    提交代码

--- Xcode/string训练/string训练/main.cpp
+++ Xcode/string训练/string训练/main.cpp
@@ -8,15 +8,22 @@
 #include <iostream>
 #include <string>
 
-
-
+using std::cout;
+using std::endl;
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     std::string s("ffapf");
     
-    std::cout << s;
-    
-    
+    std::cout << s[3];
+    s += "faf";
+    cout << s << std ::endl;
+    s.insert(3, "ds");
+    cout << s << endl;
+    s.erase(1000);
+    cout << s;
     return 0;
 }
+
+
+
