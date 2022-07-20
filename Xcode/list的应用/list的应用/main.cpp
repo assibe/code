efb74503a4e0eb2c0d9f04366a19d97f60f372f5
assibe <2775213538@qq.com>
2022-07-20 20:58:10 +0800

    提交代码

---
+++ Xcode/list的应用/list的应用/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  list的应用
+//
+//  Created by zhourenxi on 2022/7/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    return 0;
+}
