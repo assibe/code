4997a06c451c434dd0e0c9121e0176d682849a8b
assibe <2775213538@qq.com>
2022-08-04 00:40:11 +0800

    提交代码

--- Xcode/string类的模拟实现/string类的模拟实现/string.hpp
+++ Xcode/string类的模拟实现/string类的模拟实现/string.hpp
@@ -9,20 +9,58 @@
 #define string_hpp
 
 #include <stdio.h>
+#include <assert.h>
+#include <string>
 
 #endif /* string_hpp */
 
-namespace  {
+namespace zhorenxi {
 
-class zhorenxi{
+class string{
+    
 public:
     
-
+    string(const char* str = "")//拷贝构造
+    :_size(strlen(str))
+    ,_capacity(_size)
+    {
+        _str = new char[_capacity + 1];
+        strcpy(_str,str);
+        
+    }
+    
+    string(const string& s)
+    :_size(strlen(s._str))
+    ,_capacity(_size)
+    {
+        _str = new char[_capacity + 1]();
+        strcpy(_str, s._str);
+        
+    }
+    
+    string& operator=(const string &s){
+        
+        if (this != &s) {
+            
+            char* tmp = new char[s._capacity + 1]();
+			strcpy(tmp, s._str);
+			delete [] _str;
+			_str = tmp;
+			_size = s._size;
+			_capacity = s._capacity;
+			
+        }
+		return *this;
+    }
+	
+	
+	
+    
 private:
     
     char * _str;
-    size_t _size;
-    size_t _capacity;
+    size_t _size;  //有效字符个数
+    size_t _capacity;  //实际储存有效字符的空间
     
     
 };
