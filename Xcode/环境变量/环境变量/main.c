1e7a8171de92b90bd7f5e2f69489344c1dab286a
assibe <2775213538@qq.com>
2022-08-13 11:41:14 +0800

    提交代码

---
+++ Xcode/环境变量/环境变量/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  环境变量
+//
+//  Created by zhourenxi on 2022/8/13.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
