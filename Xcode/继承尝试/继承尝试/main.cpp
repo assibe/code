f657a2d81ea2cd7cfd2da2f78b709e9afd1eb3ab
assibe <2775213538@qq.com>
2022-08-21 23:07:48 +0800

    提交代码

---
+++ Xcode/继承尝试/继承尝试/main.cpp
@@ -0,0 +1,45 @@
+//
+//  main.cpp
+//  继承尝试
+//
+//  Created by zhourenxi on 2022/8/21.
+//
+
+#include <iostream>
+
+class test{
+    
+public:
+    int c = 0;
+    int b = 0;
+};
+
+class test1:public test{
+
+public:
+    int m = 0;
+};
+
+class test2:public test{
+    
+public:
+    int n = 0;
+};
+
+
+class test3: virtual  public test2,virtual public test1{
+    
+public:
+    int m = 1;
+};
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    test3 m;
+    cout << m.test2::n << endl;
+    return 0;
+}
