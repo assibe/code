37d25bd75ede0f61581e4007eccb8f70bc2491b7
assibe <2775213538@qq.com>
2022-08-28 21:41:09 +0800

    提交代码

--- Xcode/继承尝试/继承尝试/main.cpp
+++ Xcode/继承尝试/继承尝试/main.cpp
@@ -5,20 +5,19 @@
 //  Created by zhourenxi on 2022/8/21.
 //
 
-#include <iostream>
-
-class test{
-    
-public:
-    int c = 0;
-    int b = 0;
-};
-
-class test1{
-    
-public:
-    test _test;
-};
+//
+//class test{
+//
+//public:
+//    int c = 0;
+//    int b = 0;
+//};
+//
+//class test1{
+//
+//public:
+//    test _test;
+//};
 
 //class test1:public test{
 //
@@ -39,14 +38,30 @@ public:
 //    int m = 1;
 //};
 
+#include <iostream>
+#include <stdio.h>
 using namespace std;
+
+class Base{
+    
+public:
+    virtual void Fun1(){
+        
+        cout << "Fun1()"<< endl;
+    }
+    
+private:
+    int _b;
+    
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
     
-    test1 m;
-    cout << m._test.b<< endl;
-    cout << m.test1::c;
+    Base b;
+    
+    b.Fun1();
+    
+    cout << "测试";
     return 0;
 }
