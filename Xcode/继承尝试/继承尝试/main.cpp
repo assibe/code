10cf7f7248e780f8c57f953187e0e9fadb9043c0
assibe <2775213538@qq.com>
2022-08-29 00:53:08 +0800

    提交代码

--- Xcode/继承尝试/继承尝试/main.cpp
+++ Xcode/继承尝试/继承尝试/main.cpp
@@ -38,30 +38,102 @@
 //    int m = 1;
 //};
 
+//#include <iostream>
+//#include <stdio.h>
+//using namespace std;
+//
+//class Base{
+//
+//public:
+//    virtual void Fun1(){
+//
+//        cout << "Base::Fun1()"<< endl;
+//    }
+//
+//private:
+//    int _b = 4;
+//
+//};
+//
+//class Base1:public Base{
+//
+//    virtual void Fun1(){
+//
+//        cout << "Base1::Fun1()"<<endl;
+//    }
+//};
+//
+//
+//
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//
+//    Base b;
+//
+//    b.Fun1();
+//
+//    cout << "测试";
+//    return 0;
+//}
+
+//
+//#include <iostream>
+//
+//using namespace std;
+//class A
+//{
+//public:
+//  virtual void f()
+//  {
+//    cout<<"A::f()"<<endl;
+//  }
+//};
+//
+//class B : public A
+//{
+//public:
+//   virtual void f()
+//  {
+//    cout<<"B::f()"<<endl;
+//  }
+//};
+//
+//int main(){
+//
+//    A* pa = (A*)new B;
+//    pa->f();
+//    return  0;
+//}
+//
+
 #include <iostream>
-#include <stdio.h>
 using namespace std;
 
-class Base{
-    
+class A
+{
 public:
-    virtual void Fun1(){
-        
-        cout << "Fun1()"<< endl;
-    }
-    
-private:
-    int _b;
-    
+  A ():m_iVal(0){test();}
+  virtual void func() { std::cout<<m_iVal<<" ";}
+  void test(){func();}
+public:
+  int m_iVal;
 };
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+class B : public A
+{
+public:
+  B(){test();}
+  virtual void func()
+  {
+    ++m_iVal;
+    std::cout<<m_iVal<<" ";
+  }
+};
+
+int main(){
     
-    Base b;
-    
-    b.Fun1();
-    
-    cout << "测试";
-    return 0;
+  A*p = new B;
+  p->test();
+  return 0;
+
 }
---
+++ Xcode/进程尝试/进程尝试/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  进程尝试
+//
+//  Created by zhourenxi on 2022/8/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
