f178bb06ec0596b2de39a74f250fd927956d9103
assibe <2775213538@qq.com>
2022-08-30 23:47:42 +0800

    提交代码

--- Xcode/进程尝试/进程尝试/main.cpp
+++ Xcode/进程尝试/进程尝试/main.cpp
@@ -6,9 +6,12 @@
 //
 
 #include <iostream>
+#include <unistd.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc, char* argv[])
+{
+   fork();
+    (fork() && fork()) || fork();
+   fork();
     return 0;
 }
