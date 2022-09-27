cf623c114bd10ed4fbf0b4c62cc92de5215827bb
assibe <2775213538@qq.com>
2022-09-27 23:32:17 +0800

    提交代码

--- Xcode/二叉树的最近公共祖先/二叉树的最近公共祖先/main.cpp
+++ Xcode/二叉树的最近公共祖先/二叉树的最近公共祖先/main.cpp
@@ -4,11 +4,31 @@
 //
 //  Created by zhourenxi on 2022/9/23.
 //
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+class Solution {
+public:
+    unordered_map<int, TreeNode*> fa;
+    unordered_map<int, bool> vis;
+    void dfs(TreeNode* root){
+        if (root->left != nullptr) {
+            fa[root->left->val] = root;
+            dfs(root->left);
+        }
+        if (root->right != nullptr) {
+            fa[root->right->val] = root;
+            dfs(root->right);
+        }
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        fa[root->val] = nullptr;
+        dfs(root);
+        while (p != nullptr) {
+            vis[p->val] = true;
+            p = fa[p->val];
+        }
+        while (q != nullptr) {
+            if (vis[q->val]) return q;
+            q = fa[q->val];
+        }
+        return nullptr;
+    }
+};
