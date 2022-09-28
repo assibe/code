2cb1c18ea1968375a26d214e82e56210ea2ddc3e
assibe <2775213538@qq.com>
2022-09-28 11:32:25 +0800

    提交代码

---
+++ Xcode/ALV树/ALV树/AVLTreeNode.cpp
@@ -0,0 +1,9 @@
+//
+//  AVLTreeNode.cpp
+//  ALV树
+//
+//  Created by 周仁玺 on 2022/9/28.
+//
+
+#include "AVLTreeNode.hpp"
+
---
+++ Xcode/ALV树/ALV树/AVLTreeNode.hpp
@@ -0,0 +1,29 @@
+//
+//  AVLTreeNode.hpp
+//  ALV树
+//
+//  Created by 周仁玺 on 2022/9/28.
+//
+
+#ifndef AVLTreeNode_hpp
+#define AVLTreeNode_hpp
+
+#include <stdio.h>
+#include <utility>
+
+#endif /* AVLTreeNode_hpp */
+
+using std::pair;
+
+template<class K,class V>
+
+struct AVLTreeNode{
+
+    
+    
+public:
+    
+    int _bf;
+    
+    
+}
