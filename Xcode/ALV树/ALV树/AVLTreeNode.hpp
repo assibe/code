64bf559bd2d9bbdf6a2188e0d95e95f09bded2a0
assibe <2775213538@qq.com>
2022-10-01 02:41:25 +0800

    提交代码

--- Xcode/ALV树/ALV树/AVLTreeNode.hpp
+++ Xcode/ALV树/ALV树/AVLTreeNode.hpp
@@ -42,17 +42,137 @@ class AVLTree{
     typedef AVLTreeNode<K, V>  Node;
 public:
     
-private:
-    void RotateL(Node* parent){
+    bool inseert(const pair<K, V> &kv){
+        if (_root == nullptr) {
+            _root = new Node(kv);
+            _root->_bf = 0;
+            return true;
+        }
         
-        Node* subR = parent->_right;
-        Node* subL = subR-> _left;
+        Node* parent = nullptr;
+        Node* cur = _root;
         
-        parent->_parent = subR;
+        while (cur) {
+            
+            if (_root->_kv.first <= kv.first) {
+                parent = cur;
+                cur = cur->_right;
+            }
+            else if (cur->_kv.first > kv.first){
+                
+                parent = cur;
+                cur = cur ->_left;
+                
+            }
+            else{
+                return false;
+            }
+        }
         
+        cur = new Node(kv);
+        if (parent->_kv.first > kv.first) {
+            parent->_right = cur;
+        }
+        else{
+            parent->_left = cur;
+        }
+        cur->_parent = parent;
+        //按照搜索树的规则进行插入内容
+        
+        while (parent) {
+            
+            if (cur == parent->_right) {
+                parent->_bf++;
+            }
+            else{
+                parent->_bf--;
+                
+            }
+            
+            if (parent->_bf == 0) {
+                break;
+            }
+            else if (parent->_bf == 1|| parent->_bf == -1){
+                
+                cur = parent->_parent;
+            }
+            else if (parent->_bf == 2|| parent->_bf == -2){//数不平衡需要旋转
+                
+            }
+            else{
+               // 存在平衡因子，之前就不对
+                assert(false);
+            }
+        }
         
     }
     
+    
+private:
+
+    
+    void RotateL(Node* parent){
+        
+        Node* subR = parent->_right;
+        Node* subRL = subR->_left;
+        
+        parent->_right = subRL;
+        
+        if (subRL) {
+            subRL->_parent = parent;
+        }
+        
+        Node* ppNode = parent->_parent;
+        
+        subR->_left = parent;
+        parent->_parent = subR;
+        
+        if (parent == _root) {
+            _root->_parent = nullptr;
+            _root = subR;
+        }
+        else{
+            
+            if (ppNode->_left == parent) {
+                ppNode->_left = subR;
+            }
+            else{
+                ppNode->_right = subR;
+            }
+        }
+        
+    }
+    
+    void RotateR(Node* parent){
+        
+        Node* subL = parent->_left;
+        Node* subLR = subL->_right;
+        
+        parent->_right = subLR;
+        if (subLR) {//判读断是非为空
+            subLR ->_parent =parent;
+        }
+        
+        Node* pNode = parent->_parent;
+        subL = parent;
+        parent->_parent = subL;
+        
+        if (parent == _root) {
+            _root = subL;
+            _root->_parent = nullptr;
+        }
+        else{
+            subL->_parent = pNode;
+            if (pNode->_left == parent) {
+                pNode->_left = subL;
+            }
+            else{
+                pNode->_right = subL;
+            }
+            
+        }
+        
+    }
 private:
     Node* _root = nullptr;
     
