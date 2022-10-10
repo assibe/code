18529fbd1437cdd2b6493bf5a88898dedc6b44f5
assibe <2775213538@qq.com>
2022-10-11 03:28:05 +0800

    提交代码

---
+++ Xcode/哈希桶实现/哈希桶实现/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  哈希桶实现
+//
+//  Created by 周仁玺 on 2022/10/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
--- Xcode/哈希表开散列/哈希表开散列/HashTable.hpp
+++ Xcode/哈希表开散列/哈希表开散列/HashTable.hpp
@@ -25,6 +25,30 @@ enum State{
     DELETE
 };
 
+template<class K>
+
+struct DefaultHash{
+    
+    size_t operator()(const K& key){
+        
+        return (size_t)key;
+    }
+};
+
+template<>
+struct DefaultHash<string>{
+    
+    size_t operator()(const string& key){
+        
+        size_t hash = 0;
+        for (auto ch : key) {
+            hash = hash * 11 +ch;
+        }
+        return hash;
+    }
+};
+
+
 template <class K, class V>
 
 class HashDate{
@@ -34,8 +58,10 @@ class HashDate{
     
 };
 
-template <class K,class V>
+template <class K,class V,class HashFunc = DefaultHash<K>>
 class HashTable{
+    
+    typedef HashDate<K, V> Data;
     
     bool Insert(const pair<K, V>& kv){
         
@@ -70,6 +96,49 @@ class HashTable{
         _tables[hashi]._state = EXITS;
         _n++;
     }
+    
+    Data* Find(const K& key){
+        
+        if (_tables.size() == 0) {
+            
+            return nullptr;
+        }
+        
+        HashFunc hf;
+        size_t starti = hf(key);
+        starti %= _tables.size();
+        
+        size_t hashi = starti;
+        size_t i = 1;
+        
+        while (_tables[hashi]._state != EMPTY) {
+            
+            if (_tables[hashi]._state != DELETE && _tables[hashi]._kv.fisrt == key) {
+                return &_tables[hashi];
+            }
+            hashi = starti + i;
+            i++;
+            hashi %= _tables.size();
+        }
+        
+        return nullptr;
+    }
+    
+    bool Erase(const K& key){
+        
+        Data* ret = Find(key);
+        if (ret) {
+            
+            ret->_state = DELETE;
+            _n--;
+            return true;
+        }
+        else{
+            
+            return false;
+        }
+    }
+    
     
 private:
     vector<HashTable> _tables;
