6f820940d2cf9ad87272def6d1db783fd8daf33f
assibe <2775213538@qq.com>
2022-10-25 22:04:47 +0800

    提交代码

--- Xcode/哈希桶实现/哈希桶实现/main.cpp
+++ Xcode/哈希桶实现/哈希桶实现/main.cpp
@@ -6,12 +6,19 @@
 //
 
 #include <iostream>
+//#include <>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     
     
+    printf(<#const char *, ...#>);
+    
+    
+    return 0;
     
     
     
