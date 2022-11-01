9ff6d5d511c7c7355740a98e9286ee15b60f3f3b
assibe <2775213538@qq.com>
2022-11-02 03:09:01 +0800

    提交代码

--- Xcode/智能指针模拟/智能指针模拟/SmartPtr.hpp
+++ Xcode/智能指针模拟/智能指针模拟/SmartPtr.hpp
@@ -75,3 +75,100 @@ private:
     T* _ptr;
 };
 };
+
+template<class T>
+
+class unique_ptr{
+    
+    
+    unique_ptr(T* ptr)
+    :_ptr(ptr)
+    {}
+    
+    ~unique_ptr(){
+        
+        if (_ptr) {
+            delete _ptr;
+            _ptr = nullptr;
+        }
+    }
+
+    T& operator*(){
+
+        return *_ptr;
+    }
+
+    T* operator->(){
+
+        return _ptr;
+    };
+    
+    T *get(){
+        
+        return _ptr;
+    }
+    
+    
+//    unique_ptr<T>& opertor=(const unique_ptr<T>& sp) = delete;
+    unique_ptr(const unique_ptr<T>& sp) =delete;
+    unique_ptr<T>& operator=(const unique_ptr<T> &sp) = delete;
+    
+private:
+    T* _ptr;
+    
+};
+
+
+template <class T>
+
+class shared_ptr{
+    
+    void Release(){
+        
+        if (--(*_pCount) == 0 && _ptr) {
+            delete _ptr;
+            _ptr = nullptr;
+            delete _pCount;
+            _pCount = nullptr;
+        }
+    }
+    
+    ~shared_ptr(){
+        
+        Release();
+    }
+    
+    shared_ptr(T* ptr)
+    :_ptr(ptr)
+    ,_pCount(new int(1))
+    {}
+    
+    shared_ptr(const shared_ptr<T>& sp)
+    :_ptr(sp)
+    ,_pCount(sp._pCount){
+        
+        (*_pCount)++;
+    }
+    
+    T& operator*(){
+
+        return *_ptr;
+    }
+
+    T* operator->(){
+
+        return _ptr;
+    };
+    
+    T *get(){
+        
+        return _ptr;
+    }
+    
+    
+    
+private:
+    T* _ptr;
+    int* _pCount;
+    
+};
