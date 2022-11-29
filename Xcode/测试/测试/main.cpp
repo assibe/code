e1125a8ee69c3ad93e0ae4485c8bcfe827d58dd3
assibe <2775213538@qq.com>
2022-11-29 23:47:24 +0800

    提交代码

---
+++ Xcode/测试/测试/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  测试
+//
+//  Created by 周仁玺 on 2022/11/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
