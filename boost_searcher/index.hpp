aef8183447a93a7f87805473e3b8bc271ef6ee8c
assibe <2775213538@qq.com>
2023-02-28 14:58:36 +0800

    更新

--- boost_searcher/index.hpp
+++ boost_searcher/index.hpp
@@ -3,9 +3,10 @@
 
 #include <stdio.h>
 #include <vector>
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cerror>
 
 #endif /* util__hpp */
 
@@ -13,24 +14,61 @@
 
 namespace ns_index{
 
-    struct DocInfo{
+    typedef struct DocInfo{
         
         string title;
         string content;
         string url;
         int doc_id;
 
+    }DocInfo;
+    
+    struct InvertedElem{
+        uint64_t doc_id;
+        std::string word;
+        int weight;
     }
 
+    typedef  vector<InvertedElem> InvertedList;
+
+
     class Index{
-
-   
-    
+        
     private:
-    vector<DocInfo>
+        vector<DocInfo> forward_index;
+        unordered_map<string,InvertedList> inverted_index;
 
+    public: 
+        Index(){};
+        ~Index(){};
+
+  
+
+    DocInfo *GetForwardIndex(uint64_t doc_id){
+
+        if(doc_id >= forward_index.size()){
+            std::cerr << word << "doc_id out range .error" << std::endl;
+            return nullptr;
+        }
+
+        return &forward_index[doc_id];
+    }
+
+
+    DocInfo *GetInvertedList(const string &word){
+        
+        auto iter = Inverted_index.find(word);
+        if(iter == Inverted_index.end()){
+        
+            std:: cerr << word << "have no invertedList" << std::endl;
+            return nullptr;
+        }
+
+        return           
+    }
+
+    bool BuildIndex(const string &input){
+
+        return true;
+    }
 }
-}
-
-
-
