8a0bf496ba5ae8a946d35b3116b77e3eab81c67c
assibe <2775213538@qq.com>
2023-03-03 10:25:41 +0800

    upgrade

--- Xcode/测试/测试/main.cpp
+++ Xcode/测试/测试/main.cpp
@@ -5,10 +5,20 @@
 //  Created by 周仁玺 on 2023/2/26.
 //
 
+
+#include <unistd.h>
 #include <iostream>
 
+using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+  
+  
+    alarm(1);
+    int cent =  0;
+    while(1){
+        cent++;
+        std::cout<< cent << std::endl;
+    }
     return 0;
 }
