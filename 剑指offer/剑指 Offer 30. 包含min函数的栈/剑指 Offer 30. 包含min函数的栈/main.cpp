3048214052035555b163fff5285bc7d3f40bfb6c
assibe <2775213538@qq.com>
2023-03-20 09:07:00 +0800

    剑指Offer30

--- 剑指offer/剑指 Offer 30. 包含min函数的栈/剑指 Offer 30. 包含min函数的栈/main.cpp
+++ 剑指offer/剑指 Offer 30. 包含min函数的栈/剑指 Offer 30. 包含min函数的栈/main.cpp
@@ -4,11 +4,49 @@
 //
 //  Created by 周仁玺 on 2023/3/20.
 //
+#include <stack>
+using namespace std;
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    stack<int> St;
+    stack<int> minStack;
+    
+public:
+    MinStack() {
+        
+        while (!St.empty()) {
+            St.pop();
+        }
+        
+        while (!minStack.empty()) {
+            minStack.pop();
+        }
+        
+        minStack.push(INT_MAX);//空最小的栈不可以进行比较，会造成报错
+        
+    }
+    
+    void push(int x) {
+        
+        St.push(x);
+        minStack.push(std::min(minStack.top(), x));//min比较大小返回最小的那一个
+    }
+    
+    void pop() {
+        
+        St.pop();
+        minStack.pop();
+        
+    }
+    
+    
+    int top() {
+        return  St.top();
+    };
+    
+    int min() {
+        return minStack.top();
+    };
+};
