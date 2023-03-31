8a44e0c010fa895f00089958bad9b07e451d9e09
assibe <2775213538@qq.com>
2023-03-31 20:31:27 +0800

    TCP网络连接客户端

---
+++ Xcode/UDP/log.hpp
@@ -0,0 +1,41 @@
+#ifndef log_hpp
+#define log_hpp
+
+#include <cstdio>
+#include <ctime>
+#include<cstdarg>
+#include<cstring>
+#include<stdlib.h>
+#include <cassert>
+#include<cerrno>
+
+#endif
+
+// degbug notice warining fatal 
+
+
+#define DEBUG 0
+#define NOTICE 1
+#define WARINING 2
+#define FATAL 3
+
+const char *log_level[] = {"DEBUG","NOTICE","WARINING","FATAL"};
+
+void logMessage(int level,const char *format,...){
+
+    assert(level >= DEBUG);
+    assert(level <= FATAL);
+
+    char *name = getenv("USER");
+
+    char logInfo[1024];
+    va_list ap;
+    va_start (ap,format);
+    vsnprintf(logInfo,sizeof(logInfo),format,ap);
+    va_end(ap);
+    
+    FILE *out = (level == FATAL) ? stderr:stdout;
+    fprintf(out,"%s|%u|%s|%s\n",log_level[level],(unsigned int)time(nullptr),name == nullptr ? "unknow":name,logInfo);
+}
+
+
