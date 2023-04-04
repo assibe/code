72345489ae0b19219dfcc754de5caf15619e8a39
assibe <2775213538@qq.com>
2023-04-04 21:31:07 +0800

    UDP协议的书写

---
+++ Xcode/UDP/Server.cpp
@@ -0,0 +1,60 @@
+#pragma 
+
+#include <iostream>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include "Log.hpp"
+
+class Server{
+
+public:
+    Server(int port,std::string ip = " "):port_(port),ip_(ip),sockfd_(-1){
+
+    }
+
+    ~Server(){}
+
+public:
+    void Init(){
+        
+        sockfd_ = socket(AF_INET,SOCK_DGRAM,0);
+        if(sockfd_ < 0){
+            logMessage(FATAL,strerror(errno),sockfd_);
+        }
+        logMessage(DEBUG,"socket create success : %d",sockfd_);
+
+        struct sockaddr_in local;
+        
+        local.sin_family = AF_INET;
+        local.sin_port = htons(port_);
+        local.sin_addr.s_addr = ip_.empty() ? htonl(INADDR_ANY) : inet_addr(ip_.c_str());
+
+        if(bind(sockfd_,(const struct sockaddr*)&local,sockfd_) == -1){
+            logMessage(FATAL,"bind %s:%d",strerror(errno),sockfd_);
+            exit(2);
+        }
+        logMessage(DEBUG,"socket bind sucess: %d",sockfd_);
+
+    }
+
+    void start(){
+        char inbuffer[1024];
+        char outbuffer[1024];
+        while (true)
+        {
+            
+        }
+        
+    }
+    
+private:
+    std::string ip_;
+    uint16_t port_;
+    int sockfd_;
+    
+};
