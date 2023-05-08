8cb494a53365f9b7d06010704495de5b320fb100
assibe <27775213538@qq.com>
2023-05-08 17:04:03 +0800

    更新

--- boost_searcher/parser.cc
+++ boost_searcher/parser.cc
@@ -1,130 +1,204 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <boost/filesystem.hpp>
+#include "util.hpp"
 
-#include<string>
-#include<vector>
-#include<iostream>
-#include"util.hpp"
-#include<boost/filesystem.hpp>
-
-using namespace std;
-
-const std::string src_path = "data/input/";
-const std::string output = "data/raw_html/raw.txt";
+const std::string src_path = "data/input";
+const std::string output = "data/output/raw.txt";
 
 typedef struct DocInfo{
-	
-	std::string title;
-	std::string content;
-	std::string url;
+
+    std::string title;   
+    std::string content;    
+	std::string url;     
 
 }DocInfo_t;
 
-uint16_t
 
-bool EnumFile(const std::string &src_path,std::vector<std::string> *filst_list);
-bool ParseHtml(const std::vector<std::string> &files_list,std::vector<DocInfo_t> *reselts) ;
-bool SaveHtml(const DocInfo_t  &results,const std::string &output);
+bool EnumFile(const std::string &src_path, std::vector<std::string> *files_list);
+bool ParseHtml(const std::vector<std::string> &files_list, std::vector<DocInfo_t> *results);
+bool SaveHtml(const std::vector<DocInfo_t> &results, const std::string &output);
+
+int main()
+{
+    std::vector<std::string> files_list;
+   
+    if(!EnumFile(src_path, &files_list)){
+        std::cerr << "enum file name error!" << std::endl;
+        return 1;
+
+    }
 
 
-int main(){
-	
-	std::vector<std::string> filst_list;
-	if(!EnumFile(src_path,&filst_list)){
-		
-		std::cerr<<"EnumFile faile" << std::endl;
-		return 1;
-	}
+    std::vector<DocInfo_t> results;
+    if(!ParseHtml(files_list, &results)){
+        std::cerr << "parse html error" << std::endl;
+        return 2;
+    }
 
-	DocInfo_t results;	
-	if(!ParseHtml(filst_list,results)){
-		std::cerr << "parse html error" << std::endl;
-		return 2;
-	}
+    
 
-	if(!SaveHtml(results,output)){
-		std::cerr << "save error";
-		return 3;
-	};//倒入想要的容器，进行增删查找
-	
-	return 0;
+    if(!SaveHtml(results, output)){
+        std::cerr << "sava html error" << std::endl;
+        return 3;
+    }
+
+    return 0;
 }
 
-bool EnumFile(const std::string &src_path,std::vector<std::string> *files_list){
-	namespace fs = boost::filesystem;
-	fs::path root_path(src_path);	
-	if(boost::filesystem::exists(root_path)){
+bool EnumFile(const std::string &src_path, std::vector<std::string> *files_list)
+{
+    namespace fs = boost::filesystem;
+    fs::path root_path(src_path);
 
-		std::cerr<< src_path <<"not exists" << std::endl;
-		return false;
-	
-	}
+    //判断路径是否存在，不存在，就没有必要再往后走了
+    if(!fs::exists(root_path)){
+        std::cerr << src_path << " not exists" << std::endl;
+        return false;
+    }
 
-	boost::filesystem::recursive_directory_iterator end;
-	for(boost::filesystem::recursive_directory_iterator iter(root_path);iter != end;iter++){
-		if(boost::filesystem::is_regular_file(*iter)){
-			continue;
-		}
 
-		if(iter->path().extension() != ".html"){
-			continue;
-		}
+    fs::recursive_directory_iterator end;
+    for(fs::recursive_directory_iterator iter(root_path); iter != end; iter++){
 
-	}
-	
-	files_list->push_back(iter->path().string());
+        if(!fs::is_regular_file(*iter)){ 
+            continue;
+        }
+        if(iter->path().extension() != ".html"){ 
+            continue;
+        }
 
-	}
+        files_list->push_back(iter->path().string()); 
+    }
 
-bool ParseHtml(const std::vector<std::string> &files_list,std::vector<DocInfo_t> *reselts){
-	
-	for(const std::string &file:files_list){
-		
-		std::string results;
-		if(!ns_util::FileUtil::ReadFile(file,&results)){
-			continue;
-		}
-
-		DocInfo_t doc;
-
-		if(!ParseTitle(results,&doc.title)){
-			continue;
-		}
-
-		if(!ParseContent(results,&doc.content))	{
-
-			continue;
-		}
-
-		if(!ParseUrl()){
-			continue;
-		}
-		
-		results->push_back(move(doc));
-		}
-
-	return true;
-	}
-
-	
-
-bool SaveHtml(const DocInfo_t  &results, const std::string &output){
-
-	#define SEP '\3'
-	ofstream out(output,std::ios::put | std::ios::binary);
-	if(!out.is_open()){
-		std::cerr << "open" << output << "failed" <<std::endl;
-		return false;
-	}
-
-	for(auto &item : results){
-		
-		std::string out_string;
-		out_string = item.title;
-		out_string += SEP;
-		out_string += item.content;
-		out_string += item.url; 
-
-	}
-
-	out.close;
-	return true;
+    return true;
 }
+
+static bool ParseTitle(const std::string &file, std::string *title)
+{
+    std::size_t begin = file.find("<title>");
+    if(begin == std::string::npos){
+        return false;
+    }
+    std::size_t end = file.find("</title>");
+    if(end == std::string::npos){
+        return false;
+    }
+
+    begin += std::string("<title>").size();
+
+    if(begin > end){
+        return false;
+    }
+    *title = file.substr(begin, end - begin);
+    return true;
+}
+
+static bool ParseContent(const std::string &file, std::string *content)
+{
+    
+    enum status{
+        LABLE,
+        CONTENT
+    };
+
+    enum status s = LABLE;
+    for( char c : file){
+        switch(s){
+            case LABLE:
+                if(c == '>') s = CONTENT;
+                break;
+            case CONTENT:
+                if(c == '<') s = LABLE;
+                else {
+
+                    if(c == '\n') c = ' ';
+                    content->push_back(c);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    return true;
+}
+
+static bool ParseUrl(const std::string &file_path, std::string *url)
+{
+    std::string url_head = "https://www.boost.org/doc/libs/1_78_0/doc/html";
+    std::string url_tail = file_path.substr(src_path.size());
+
+    *url = url_head + url_tail;
+    return true;
+}
+
+
+static void ShowDoc( const DocInfo_t &doc)
+{
+    std::cout << "title: " << doc.title << std::endl;
+    std::cout << "content: " << doc.content << std::endl;
+    std::cout << "url: " << doc.url << std::endl;
+}
+
+bool ParseHtml(const std::vector<std::string> &files_list, std::vector<DocInfo_t> *results)
+{
+    for(const std::string &file : files_list){
+       
+
+        std::string result;
+        if(!ns_util::FileUtil::ReadFile(file, &result)){
+            continue;
+        }
+        DocInfo_t doc;
+
+        //2. 解析指定的文件，提取title
+        if(!ParseTitle(result, &doc.title)){
+            continue;
+        }
+
+		
+        if(!ParseContent(result, &doc.content)){
+            continue;
+        }
+
+        if(!ParseUrl(file, &doc.url)){
+            continue;
+        }
+
+        results->push_back(std::move(doc)); 
+    }
+    return true;
+}
+
+bool SaveHtml(const std::vector<DocInfo_t> &results, const std::string &output)
+{
+#define SEP '\3'
+    
+
+    std::ofstream out(output, std::ios::out | std::ios::binary);
+    if(!out.is_open()){
+        std::cerr << "open " << output << " failed!" << std::endl;
+        return false;
+    }
+
+    
+    for(auto &item : results){
+        std::string out_string;
+        out_string = item.title;
+        out_string += SEP;
+        out_string += item.content;
+        out_string += SEP;
+        out_string += item.url;
+        out_string += '\n';
+
+        out.write(out_string.c_str(), out_string.size());
+    }
+
+    out.close();
+
+    return true;
+}
+
+
--- boost_searcher/util.hpp
+++ boost_searcher/util.hpp
@@ -1,83 +1,120 @@
-#ifndef util__hpp
-#define util__hpp
+#pragma once
 
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <mutex>
+#include <unordered_map>
 #include <boost/algorithm/string.hpp>
-#include "/inc/cppjieba/Jieba.hpp"
-#include<iostream>
-#include<fstream>
-#include<string>
-#endif /* util__hpp */
-
-ns_util::StringUtil::CuString(line,&results,sep);
+// #include "log.hpp"
+// #include "cppjieba/Jieba.hpp"
 
 namespace ns_util{
-    
     class FileUtil{
         public:
-            static bool ReadFile(const std::string &file_path,std::string *out){
-                
-               for(const std::string &file:files_list){
-
-                 std::ifstream in(file_path,std::ios::in);
-                if(in.is_open()){
-
-                    std::cerr <<"open file"<<std::endl;
+            static bool ReadFile(const std::string &file_path, std::string *out)
+            {
+                std::ifstream in(file_path, std::ios::in);
+                if(!in.is_open()){
+                    std::cerr << "open file " << file_path << " error" << std::endl;
                     return false;
-
-                } 
-
-               }      
+                }
 
                 std::string line;
-                while(std::getline(in,line)){
-                *out += line;
+                while(std::getline(in, line)){
+                    *out += line;
                 }
-                
+
                 in.close();
                 return true;
             }
-    }
-
-}
-
-static bool ParseContent(const std::string &file,std::string *content){
-
-    enum{
-        LABLE;
-        CONTENT;
     };
+    // class StringUtil{
+    //     public:
+    //         static void Split(const std::string &target, std::vector<std::string> *out, const std::string &sep)
+    //         {
+    //             boost::split(*out, target, boost::is_any_of(sep), boost::token_compress_on);
+    //         }
+    // };
 
-    enum status s = LABLE;
-    for(char c:file){
-        switch(s){
-            case LABLE:
-            if(c == '>') s = CONTENT;;
-            break;
-            }
+    // const char* const DICT_PATH = "./dict/jieba.dict.utf8";
+    // const char* const HMM_PATH = "./dict/hmm_model.utf8";
+    // const char* const USER_DICT_PATH = "./dict/user.dict.utf8";
+    // const char* const IDF_PATH = "./dict/idf.utf8";
+    // const char* const STOP_WORD_PATH = "./dict/stop_words.utf8";
 
-            case CONTENT:
-            if(c == '<')
-             s = LABLE;
-            else{
+    // class JiebaUtil{
+    //     private:
+    //         cppjieba::Jieba jieba;
+    //         std::unordered_map<std::string, bool> stop_words;
+    //     private:
+    //         JiebaUtil():jieba(DICT_PATH, HMM_PATH, USER_DICT_PATH, IDF_PATH, STOP_WORD_PATH)
+    //         {}
+    //         JiebaUtil(const JiebaUtil&) = delete;
 
-                if(c == '\n') c = ' ' ;
-                content->push_back(c);  
-            }
-            break;
-            default:
-                break;
+    //         static JiebaUtil *instance;
+    //     public:
 
-        }
-    }
-    return true;
+    //         static JiebaUtil* get_instance()
+    //         {
+    //             static std::mutex mtx;
+    //             if(nullptr == instance){
+    //                 mtx.lock();
+    //                 if(nullptr == instance){
+    //                     instance = new JiebaUtil();
+    //                     instance->InitJiebaUtil();
+    //                 }
+    //                 mtx.unlock();
+    //             }
+
+    //             return instance;
+    //         }
+
+    //         void InitJiebaUtil()
+    //         {
+    //             std::ifstream in(STOP_WORD_PATH);
+    //             if(!in.is_open()){
+    //                 LOG(FATAL, "load stop words file error");
+    //                 return;
+    //             }
+
+    //             std::string line;
+    //             while(std::getline(in, line)){
+    //                 stop_words.insert({line, true});
+    //             }
+
+    //             in.close();
+    //         }
+
+    //         void CutStringHelper(const std::string &src, std::vector<std::string> *out)
+    //         {
+    //             jieba.CutForSearch(src, *out);
+    //             for(auto iter = out->begin(); iter != out->end(); ){
+    //                 auto it = stop_words.find(*iter);
+    //                 if(it != stop_words.end()){
+    //                     iter = out->erase(iter);
+    //                 }
+    //                 else{
+    //                     iter++;
+    //                 }
+    //             }
+    //         }
+
+    //     public:
+    //         static void CutString(const std::string &src, std::vector<std::string> *out)
+    //         {
+    //             ns_util::JiebaUtil::get_instance()->CutStringHelper(src, out);
+    //         }
+    // };
+
+    // JiebaUtil *JiebaUtil::instance = nullptr;
 }
 
-static bool ParseHtml(const std::string &file_path,std::string *url){
 
-        std::string tail_head = "https://www.boost.org/doc/libs/1_78_0/doc/html";
-                std::string tail = file_path.substr(src_path.size());
 
-                *url = _url_head + url_tail;
-                return true;
-        }
+
+
+
+
+
